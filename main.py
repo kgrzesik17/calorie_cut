fb07264996815df9d3dcb325e843543463b6389d
Kacper <kgrzesik17@gmail.com>
2025-03-08 17:21:31 +0100

    added imperial to metric conversion

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 def calculate_weigth(weigth):
-    return weight * 0.4535
+    return float(weight) * 0.4535
 
 def calculate_height(height):
-    pass
+    feet = height.split("'")[0]
+    inch = height.split("'")[1]
+    
+    height = float(feet) * 30.48 + float(inch) * 2.54
+
+    return height
 
 units = 0
 
 while units not in [1, 2]:
-    units = input('What unit are you using? [1 - metric, 2 - imperial]')
+    units = input('What unit are you using? [1 - metric, 2 - imperial]: ')
 
     if units == 1:
         height = input('Enter your height in [centimeters]: ')
         weigth = input('Enter your weight in [kilograms]: ')
     else:
-        height = input('Enter your height in [feet\'inches]')
-        weight = input('Enter your weight in [lbs]')
+        height = input('Enter your height in [feet\'inches]: ')
+        weight = input('Enter your weight in [lbs]: ')
+
+        height = calculate_height(height)
+        weight = calculate_weigth(weight)
     
 def main(height, weight):
+    pass
