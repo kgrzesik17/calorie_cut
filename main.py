fdc946701913da46fc9d070e7c6ecd7636b533ee
Kacper <kgrzesik17@gmail.com>
2025-03-08 21:07:21 +0100

    accounted for daily activit

--- main.py
+++ main.py
@@ -19,16 +19,33 @@ def calculate_metabolism(height, weight, age, sex):
 
     return metabolism
 
-def calculate_calories(metabolism, weight, days, goal):
+def calculate_calories(metabolism, weight, days, goal, activity):
     goal_difference = weight - goal
     calorie_deficit_overall = goal_difference * 9 * 1000
     calorie_deficit_per_day =  calorie_deficit_overall / days
 
-    diet = metabolism - calorie_deficit_per_day
+    match activity:
+        case 1:
+            activity_number = 1.2
+        case 2:
+            activity_number = 1.375
+        case 3:
+            activity_number = 1.55
+        case 4:
+            activity_number = 1.725
+        case 5:
+            activity_number = 1.9
 
-    print('Your daily metabolism is calculated to be: '+ str(metabolism) + ' calories per day.')
-    print('You need to keep a ' + str(calorie_deficit_per_day) + ' calorie deficit per day.')
-    print('That means you have to eat no more than ' + str(diet) + ' calories per day for ' + str(days) + ' days to reach your goal.')
+
+    tdee = metabolism * activity_number
+
+
+    diet = tdee - calorie_deficit_per_day
+
+    print('Your daily metabolism is calculated to be: '+ str(round(metabolism)) + ' calories per day.')
+    print('With all the daily activities, that comes to: '+ str(round(tdee)) + ' calories per day.')
+    print('You need to keep a ' + str(round(calorie_deficit_per_day)) + ' calorie deficit per day.')
+    print('That means you have to eat no more than ' + str(round(diet)) + ' calories per day for ' + str(days) + ' days to reach your goal.')
 
 units = 0
 
@@ -51,10 +68,15 @@ while units not in ['1', '2']:
 
     age = int(input('Enter your age: '))
     sex = input('Enter your sex [M/F]: ').lower()
+    activity = int(input('Select your activity level:\n' +
+    '[1] - sedentary\n' +
+    '[2] - Lightly active\n' +
+    '[3] - Moderatly active\n' +
+    '[4] - Very active\n' +
+    '[5] - Super active\n'))
 
     days = int(input('Enter in how many days do you want to reach your goal: '))
 
     metabolism = calculate_metabolism(height, weight, age, sex)
 
-    calculate_calories(metabolism, weight, days, goal)
-    
+    calculate_calories(metabolism, weight, days, goal, activity)
