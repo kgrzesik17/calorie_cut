0e0a28eb774532dc0f21ebf7f354b9ed6dde78e1
Kacper <kgrzesik17@gmail.com>
2025-03-08 19:57:24 +0100

    the skeleton is ready. need to work on numbers

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def calculate_metabolism(height, weight, age, sex):
 
     return metabolism
 
+def calculate_calories(metabolism, weight, days, goal):
+    goal_difference = weight - goal
+    calorie_deficit_overall = goal_difference * 9 * 1000
+    calorie_deficit_per_day =  calorie_deficit_overall / days
+
+    diet = metabolism - calorie_deficit_per_day
+
+    print('Your daily metabolism is calculated to be: '+ str(metabolism) + ' calories per day.')
+    print('You need to keep a ' + str(calorie_deficit_per_day) + ' calorie deficit per day.')
+    print('That means you have to eat no more than ' + str(diet) + ' calories per day for ' + str(days) + ' days to reach your goal.')
+
 units = 0
 
 while units not in ['1', '2']:
@@ -27,20 +38,23 @@ while units not in ['1', '2']:
     if units == '1':
         height = float(input('Enter your height in [centimeters]: '))
         weight = float(input('Enter your weight in [kilograms]: '))
-        goal = input('Enter how much weight do you want to lose [kilograms]')
+        goal = float(input('Enter your goal weight [kilograms]: '))
 
     else:
         height = float(input('Enter your height in [feet\'inches]: '))
         weight = float(input('Enter your weight in [lbs]: '))
-        goal = input('Enter how much weight do you want to lose [lbs]: ')
+        goal = float(input('Enter your goal weight [lbs]: '))
 
         height = calculate_height(height)
         weight = calculate_weigth(weight)
+        goal = calculate_weigth(goal)
 
     age = int(input('Enter your age: '))
     sex = input('Enter your sex [M/F]: ').lower()
 
-    days = input('Enter in how many days do you want to reach your goal: ')
+    days = int(input('Enter in how many days do you want to reach your goal: '))
 
     metabolism = calculate_metabolism(height, weight, age, sex)
+
+    calculate_calories(metabolism, weight, days, goal)
     
